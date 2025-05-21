67224472963fcc94c69144cd3eaf3a32b3f18134
MohammadBehdad MohammadiDamnab <mohammadbehdadmohammadi@gmail.com>
2025-05-20 23:22:35 -0700

    Update sketch_may20a.ino

--- sketch_may20a/sketch_may20a.ino
+++ sketch_may20a/sketch_may20a.ino
@@ -1,9 +1,42 @@
+int trig = 9;
+int echo = 8;
+int duration;
+float distance;
+float meter; 
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  pinMode(echo, INPUT);
+  delay(6000);
+  Serial.println("Distance:");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH);
+  
+  if(duration>=38000){
+    Serial.print("Out range");
+  }
+  else
+  {
+    distance = duration/58;
+    Serial.print(distance);
+    Serial.print("cm");
+    meter=distance/100;
+    Serial.print("\t");
+    Serial.print(meter);
+    Serial.println("m");
+ }
+ 
+ delay(1000)
 
 }

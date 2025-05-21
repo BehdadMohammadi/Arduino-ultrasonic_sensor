f0f22da2e1e9c240d1735c7c2bcc380c261a5180
MohammadBehdad MohammadiDamnab <mohammadbehdadmohammadi@gmail.com>
2025-05-21 01:11:29 -0700

    Update sketch_may20a.ino

--- sketch_may20a/sketch_may20a.ino
+++ sketch_may20a/sketch_may20a.ino
@@ -10,7 +10,8 @@ int flag = 0;
 
 boolean toggle1 = 0;
 
-void setup() {
+void setup()
+{
   
   Serial.begin(9600);
   pinMode(trig, OUTPUT);
@@ -23,33 +24,23 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(echo), measure, CHANGE);
 
   cli();
-  //set timer1 interrupt at 1Hz
-  TCCR1A = 0;// set entire TCCR1A register to 0
-  TCCR1B = 0;// same for TCCR1B
-  TCNT1  = 0;//initialize counter value to 0
-  // set compare match register for 1hz increments
-  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
-  // turn on CTC mode
+  
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A = 15624;
   TCCR1B |= (1 << WGM12);
-  // Set CS12 and CS10 bits for 1024 prescaler
   TCCR1B |= (1 << CS12) | (1 << CS10);  
-  // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
 
   sei();
 
-  
-  delay(6000);
+  delay(3000);
 }
 
-void loop() {
+void loop()
+{
   
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  
-  delay(1000);
-
 }
 
 void measure()
@@ -73,17 +64,9 @@ void measure()
   }
 }
 
-
 ISR(TIMER1_COMPA_vect)
 {
-  //timer1 interrupt 1Hz toggles pin 13 (LED)
-//generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
-  if (toggle1){
-    digitalWrite(13,HIGH);
-    toggle1 = 0;
-  }
-  else{
-    digitalWrite(13,LOW);
-    toggle1 = 1;
-  }
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
 }
